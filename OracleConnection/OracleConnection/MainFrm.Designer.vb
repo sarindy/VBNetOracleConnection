0021618f98425f9363de671e4f7ce169bf81b0a1
sarindy <sarindyo@yahoo.com>
2015-02-04 10:45:10 +0700

    Rename Solution

--- OracleConnection/OracleConnection/MainFrm.Designer.vb
+++ OracleConnection/OracleConnection/MainFrm.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class MainFrm
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,9 +22,17 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.SuspendLayout()
+        '
+        'MainFrm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Name = "MainFrm"
+        Me.Text = "Main Form"
+        Me.ResumeLayout(False)
+
     End Sub
 
 End Class
---
+++ OracleConnection/OracleConnection/MainFrm.vb
@@ -0,0 +1,29 @@
+﻿Imports Oracle.DataAccess.Client
+Public Class MainFrm
+
+    Private Sub MainFrm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim connStr As String
+
+        connStr = "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=10.178.192.19)(PORT=1521)))" _
+                + "(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=DB1)));User Id=ucb;Password=qwe;"
+
+        Try
+            Dim con As New OracleConnection(connStr)
+            con.Open()
+            MsgBox("Suck")
+            Dim sql As String
+            Sql = "select description from acct_code"
+            Dim cmd As New Oracle.DataAccess.Client.OracleCommand(sql, con)
+            cmd.CommandType = CommandType.Text
+
+            Dim dr As Oracle.DataAccess.Client.OracleDataReader
+            dr = cmd.ExecuteReader
+            dr.Read()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+
+    End Sub
+End Class
